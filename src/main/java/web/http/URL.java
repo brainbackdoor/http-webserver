8b432be5c69c85950ac1dbf399413063f786d264
brainbackdoor <brainbackdoor@gmail.com>
2019-08-11 16:14:09 +0900

    feat(URL): Check the UserName and Password defaults.

--- src/main/java/web/http/URL.java
+++ src/main/java/web/http/URL.java
@@ -14,6 +14,25 @@ public class URL {
     public URL(String input) {
         String[] splitedInput = input.split("://");
         this.scheme = Scheme.of(splitedInput[0]);
+        if(isRequiredToUserData()){
+            setUserData(splitedInput);
+        }
+    }
+
+    private void setUserData(String[] detachedScheme) {
+        if(!isAnonymous(detachedScheme[1])) {
+            detachedScheme = detachedScheme[1].split("@");
+            String[] userData = detachedScheme[0].split(":");
+            this.userName = new UserName(userData[0]);
+            this.password = new Password(userData[1]);
+            return;
+        }
+        this.userName = new UserName();
+        this.password = new Password();
+    }
+
+    private boolean isAnonymous(String detachedScheme) {
+        return !detachedScheme.contains("@");
     }
 
     public boolean isRequiredToUserData() {
@@ -26,5 +45,13 @@ public class URL {
 
     public int getPortNumber() {
         return this.scheme.getPortNumber();
+    }
+
+    public UserName getUserName() {
+        return userName;
+    }
+
+    public Password getPassword() {
+        return password;
     }
 }
--- src/test/java/web/http/URLTest.java
+++ src/test/java/web/http/URLTest.java
@@ -30,6 +30,25 @@ class URLTest {
         assertThat(actualFalseUrl.isRequiredToUserData()).isFalse();
     }
 
-    //TODO: UserName과 Password는 ':'로 구분하고, '@'문자로 URL로부터 UserName과 Password 컴포넌트를 분리한다.
+    @Test
+    @DisplayName("UserName과 Password는 ':'로 구분하고, '@'문자로 URL로부터 UserName과 Password 컴포넌트를 분리한다.")
+    void splitUserData() {
+        String given = "ftp://joe:joespasswd@www.joes-hardware.com:80/index.html";
+
+        URL url = new URL(given);
+        assertThat(url.getUserName()).isEqualTo(new UserName("joe"));
+        assertThat(url.getPassword()).isEqualTo(new Password("joespasswd"));
+    }
+
+    @Test
+    @DisplayName("UserName과 Password 기본값을 확인한다.")
+    void splitUserDataDefault() {
+        String given = "ftp://www.joes-hardware.com:80/index.html";
+
+        URL url = new URL(given);
+        assertThat(url.getUserName()).isEqualTo(new UserName());
+        assertThat(url.getPassword()).isEqualTo(new Password());
+    }
+
     //TODO: QueryString은 '?' 문자로 URL과 구분된다.
 }
