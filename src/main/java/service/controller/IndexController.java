8f9ef99928c5ce898ebaf33e525e7591d302dbea
brainbackdoor <brainbackdoor@gmail.com>
2020-01-21 13:28:50 +0900

    chore: test code gruru

--- src/main/java/service/controller/IndexController.java
+++ src/main/java/service/controller/IndexController.java
@@ -15,6 +15,7 @@ public class IndexController extends AbstractController {
 
     @Override
     public void doGet(RequestMessage request, ResponseMessage response) {
+
         response.setResponseLine(ResponseLine.builder().statusCode(StatusCode.OK).version(Version.HTTP_1_1).build());
         response.setHeaders(Headers.of(Lists.list("Location: /templates/index.html")));
         response.setEntityBody(EntityBody.of("Please go to our partner site,\nwww.gentle-grooming.com"));
